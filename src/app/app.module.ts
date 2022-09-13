247d6036954ee3f5ca5d4ea5619223fef2f04924
prof-amer <amarzuki@wisc.edu>
2022-09-14 02:06:30 +0800

    Added auth and tasks components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule
