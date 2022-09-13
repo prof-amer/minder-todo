e52fa241a66a881b986bdf82135ddc4c83a6f782
prof-amer <amarzuki@wisc.edu>
2022-09-14 03:13:45 +0800

    refactored code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-
-  ngOnInit(){
-  //  init data fetch
-  }
-
-
+export class AppComponent {
   title = 'minder-todo';
 }
