4ab09f78531ec8abcce4d8f870d5a5a401eb1322
prof-amer <amarzuki@wisc.edu>
2022-09-17 14:39:30 +0800

    initialized new task dialog component

---
+++ src/app/components/tasks/new-task-dialog/new-task-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-task-dialog',
+  templateUrl: './new-task-dialog.component.html',
+  styleUrls: ['./new-task-dialog.component.css']
+})
+export class NewTaskDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
